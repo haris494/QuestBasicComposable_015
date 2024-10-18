a1249bdc8d08f66a68bc65154268923f9521a53e
haris49 <hariis.459@gmail.com>
2024-10-18 19:38:21 +0700

    first commit

---
+++ app/src/main/java/com/example/appjack/MainActivity.kt
@@ -0,0 +1,120 @@
+package com.example.appjack
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberTopAppBarState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.appjack.ui.theme.AppjackTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AppjackTheme  {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    BasicCompose(
+
+                        haris = Modifier.padding(innerPadding)
+
+
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+//menambahkan fungsi @Preview untuk menampilkan komponen BasicCompose dengan latar belakang di dalam editor
+@Composable
+fun BasicCompose(haris: Modifier = Modifier){
+
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+
+
+
+
+        Text(
+            text = "Login", modifier =  haris,
+            fontSize = 40.sp,
+            color = Color.Green,
+            fontWeight = FontWeight.Bold
+            )
+
+        Text(text = "Ini adalah halaman login",
+        fontSize = 25.sp,
+        color = Color.Blue,
+        fontFamily = FontFamily.Serif
+        )
+
+        Image(
+            painter = painterResource(id = R.drawable.images),
+            contentDescription = null,
+            modifier = Modifier.size(250.dp)
+        )
+
+        Text(text = "Nama",
+            fontSize = 30.sp,
+            color = Color.Blue,
+            fontWeight = FontWeight.Bold
+            )
+
+        Text(text = "Haris Shihab Dzul Firdausi",
+            fontSize = 25.sp,
+            color = Color.Blue,
+            fontFamily = FontFamily.Default
+            )
+
+        Text(text = "20220140015",
+            fontSize = 25.sp,
+            color = Color.Black,
+            fontWeight = FontWeight.Bold
+            )
+
+        Image(
+            painter = painterResource(id = R.drawable.haris),
+            contentDescription = null,
+            modifier = Modifier.size(250.dp)
+        )
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AppjackTheme {
+        Greeting("Android", modifier = Modifier)
+    }
+}
